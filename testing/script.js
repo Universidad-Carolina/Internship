5558e8f2f6cfecf1fef5dbeec706594060b6acf6
RTcastillo <luis.torres@ucarolina.edu.mx>
2024-05-03 00:41:57 -0600

    Update movie search title and styles

--- testing/script.js
+++ testing/script.js
@@ -1,14 +1,31 @@
-document.getElementsByClassName('searchBtn')[0].addEventListener('click', function () {
+document.getElementsByClassName('searchBtn')[0].addEventListener('click', searchMovie);
+
+document.getElementsByClassName('searchInput')[0].addEventListener('keypress', function (event) {
+  if (event.key === 'Enter') {
+    searchMovie();
+  }
+});
+
+function searchMovie() {
   const searchInput = document.getElementsByClassName('searchInput')[0].value;
 
   if (searchInput.trim() !== '') {
     fetchMovie(searchInput);
   }
-});
+}
 
 function fetchMovie(title) {
   const apiKey = '96dc73e3';
-  const apiUrl = `https://www.omdbapi.com/?apikey=${apiKey}` + '&t=' + encodeURI(encodeSpacesAsPlus(title));
+  const yearRegex = /\b\d{4}\b/;
+  let year = '';
+
+  const matches = title.match(yearRegex);
+  if (matches) {
+    year = matches[0];
+    title = title.replace(year, '').trim();
+  }
+
+  const apiUrl = `https://www.omdbapi.com/?apikey=${apiKey}&t=${encodeURI(encodeSpacesAsPlus(title))}&plot=full${year ? `&y=${year}` : ''}`;
 
   console.log(apiUrl);
 
@@ -35,6 +52,7 @@ function encodeSpacesAsPlus(title) {
 function displayMovieDetails(movie) {
   const moviePoster = document.getElementsByClassName('moviePoster')[0];
 
+  console.log(movie.Ratings[0]);
   if (movie.Poster !== 'N/A') {
     moviePoster.innerHTML = `<img src="${movie.Poster}" alt="Movie Poster">`;
   } else {
@@ -43,10 +61,31 @@ function displayMovieDetails(movie) {
 
   document.getElementsByClassName('movieTitle')[0].innerText = movie.Title;
   document.getElementsByClassName('moviePlot')[0].innerText = movie.Plot;
-  document.getElementsByClassName('movieActors')[0].innerText = 'Actors: ' + movie.Actors;
-  document.getElementsByClassName('movieDirector')[0].innerText = 'Director: ' + movie.Director;
-  document.getElementsByClassName('movieGenre')[0].innerText = 'Genre: ' + movie.Genre;
-  document.getElementsByClassName('movieReleased')[0].innerText = 'Released: ' + movie.Released;
-  document.getElementsByClassName('movieRuntime')[0].innerText = 'Runtime: ' + movie.Runtime;
-  document.getElementsByClassName('movieRating')[0].innerText = 'Rating: ' + movie.imdbRating;
+
+  document.getElementsByClassName('actorsText')[0].innerText = 'Actors: ';
+  document.getElementsByClassName('movieActors')[0].innerHTML = movie.Actors;
+  document.getElementsByClassName('directorText')[0].innerText = 'Director: ';
+  document.getElementsByClassName('movieDirector')[0].innerText = movie.Director;
+  document.getElementsByClassName('writersText')[0].innerText = 'Writers: ';
+  document.getElementsByClassName('movieWriters')[0].innerText = movie.Writer;
+
+  document.getElementsByClassName('movieReleased')[0].innerText = movie.Year;
+  document.getElementsByClassName('movieRuntime')[0].innerText = convertRuntime(movie.Runtime); // Convert runtime format
+
+  document.getElementsByClassName('movieRated')[0].innerText = movie.Rated;
+
+  document.getElementsByClassName('movieRatings')[0].innerText = 'Ratings:';
+  document.getElementsByClassName('imbdSource')[0].innerText = movie.Ratings[0].Source;
+  document.getElementsByClassName('imbdRating')[0].innerText = movie.Ratings[0].Value;
+  document.getElementsByClassName('rottenSource')[0].innerText = movie.Ratings[1].Source;
+  document.getElementsByClassName('rottenRating')[0].innerText = movie.Ratings[1].Value;
+  document.getElementsByClassName('metacriticSource')[0].innerText = movie.Ratings[2].Source;
+  document.getElementsByClassName('metacriticRating')[0].innerText = movie.Ratings[2].Value;
+}
+
+function convertRuntime(runtime) {
+  const minutes = parseInt(runtime);
+  const hours = Math.floor(minutes / 60);
+  const remainingMinutes = minutes % 60;
+  return `${hours}H${remainingMinutes}M`;
 }
